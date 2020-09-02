b2e96ba1419fc6bcf6ee9c0a890d357523e8033c
Ben W <bwiedeback@gmail.com>
2020-09-02 16:03:02 -0600

    unpackaged types, images working

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -1,9 +1,9 @@
 export default class Pokemon{
-  constructor({ _id = "", name, types, img = "https://pokemon-overdose.com/wp-content/uploads/132-640x640.png", weight, height}){
+  constructor({ _id = "", name, types, sprites, img , weight, height}){
     this.id = _id
     this.name = name
     this.types = types
-    this.img = img
+    this.img = img || (sprites.front_shiny || "https://pokemon-overdose.com/wp-content/uploads/132-640x640.png")
     this.weight = weight
     this.height = height
   }
@@ -13,17 +13,26 @@ export default class Pokemon{
     <div class="card-body">
       <img class="card-img-top" src="${this.img}">
       <h4 class="card-title">${this.name.charAt(0).toUpperCase()+this.name.slice(1)}</h4>
-      <p class="card-text">Types: ${this.types} </p>
+      <p class="card-text">Types: ${this.Types} </p>
       <p class="card-text">weight: ${this.weight}</p>
       <p class="card-text">height: ${this.height}</p>
       ${this.Button}
     </div>
     `
   }
+
+  get Types(){
+    let template = '| ' 
+    this.types.forEach(elem => {
+      template+= elem.type.name+' | '
+    });
+    return template
+  }
+
   get Button() {
     if (this.id) {
       return `<button onclick="app.myPokemonsController.removePokemon()" class="btn btn-danger">Remove</button>`
     }
-    return `<button onclick="app.myPokemonsController.addPokemon()" class="btn btn-success">Add Pokemon</button>`
+    return `<button onclick="app.myPokemonsController.addPokemon()" class="btn btn-primary">Add Pokemon</button>`
   }
 }
